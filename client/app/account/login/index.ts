26adc4f286f57c75e9f5610b7c471773f87537da
= <=>
2016-12-09 00:09:12 +0700

    cleaning up

--- client/app/account/login/index.ts
+++
@@ -1,7 +0,0 @@
-'use strict';
-const angular = require('angular');
-import LoginController from './login.controller';
-
-export default angular.module('geocrowdApp.login', [])
-  .controller('LoginController', LoginController)
-  .name;
